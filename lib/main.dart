2dd13b9e27c59d21f4fe96f24642f09aefa86ae3
thisisstreamliner <thisisstreamliner@gmail.com>
2020-08-19 16:55:13 +0300

    Adding an App Theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:chat_app/screens/auth_screen.dart';
-import 'package:chat_app/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +11,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Chat',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        backgroundColor: Colors.pink,
+        accentColor: Colors.deepPurple,
+        accentColorBrightness: Brightness.dark,
+        buttonTheme: ButtonTheme.of(context).copyWith(
+          buttonColor: Colors.pink,
+          textTheme: ButtonTextTheme.primary,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        ),
       ),
       home: AuthScreen(),
     );
