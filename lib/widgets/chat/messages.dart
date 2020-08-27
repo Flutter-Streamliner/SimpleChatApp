13904a98ee42a2081ea7f19d16ef307b8aae5f4c
thisisstreamliner <thisisstreamliner@gmail.com>
2020-08-27 10:27:42 +0300

    Improving Chat Bubble Messages

--- lib/widgets/chat/messages.dart
+++ lib/widgets/chat/messages.dart
@@ -1,5 +1,6 @@
 import 'package:chat_app/widgets/chat/message_bubble.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Messages extends StatelessWidget {
@@ -20,8 +21,12 @@ class Messages extends StatelessWidget {
         return ListView.builder(
           reverse: true,
           itemCount: chatDocuments.length,
-          itemBuilder: (context, index) =>
-              MessageBubble(chatDocuments[index].get('text')),
+          itemBuilder: (context, index) => MessageBubble(
+            chatDocuments[index].get('text'),
+            chatDocuments[index].get('userId') ==
+                FirebaseAuth.instance.currentUser.uid,
+            key: ValueKey(chatDocuments[index].documentID),
+          ),
         );
       },
     );
